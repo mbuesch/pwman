3114a8ba59d5424000ed45c57079f3ba262e8526
Michael Buesch <m@bues.ch>
2011-08-28 17:35:15 +0200

    Implement 'undo' and 'redo'
    Signed-off-by: Michael Buesch <m@bues.ch>

--- pwman.py
+++ pwman.py
@@ -118,6 +118,55 @@ def fileExists(path):
 		raise CSQLError("fileExists(): " + str(e))
 	return True
 
+class UndoCommand(object):
+	def __init__(self, doCommand, undoCommand):
+		self.doCommand = doCommand
+		self.undoCommand = undoCommand
+
+class UndoStack(object):
+	def __init__(self, limit=16):
+		self.limit = limit
+		self.frozen = 0
+		self.clear()
+
+	def __stackAppend(self, stack, c):
+		stack.append(c)
+		while len(stack) > self.limit:
+			stack.pop(0)
+
+	def do(self, doCommand, undoCommand):
+		if self.frozen:
+			return
+		c = UndoCommand(doCommand, undoCommand)
+		self.__stackAppend(self.undoStack, c)
+		self.redoStack = []
+
+	def undo(self):
+		if not self.undoStack:
+			return None
+		c = self.undoStack.pop()
+		self.__stackAppend(self.redoStack, c)
+		return c
+
+	def redo(self):
+		if not self.redoStack:
+			return None
+		c = self.redoStack.pop()
+		self.__stackAppend(self.undoStack, c)
+		return c
+
+	def clear(self):
+		self.undoStack = []
+		self.redoStack = []
+
+	def freeze(self):
+		assert(self.frozen >= 0)
+		self.frozen += 1
+
+	def thaw(self):
+		self.frozen -= 1
+		assert(self.frozen >= 0)
+
 class PWManEntry(object):
 	Undefined = None
 
@@ -155,18 +204,21 @@ class PWManEntry(object):
 
 class PWMan(CryptSQL, Cmd):
 	class Error(Exception): pass
+	class CommandError(Exception): pass
 	class Quit(Exception): pass
 
 	DB_TYPE		= "PWMan database"
 	DB_VER		= "0"
 
-	def __init__(self, filename, passphrase):
+	def __init__(self, filename, passphrase, commitClearsUndo=False):
 		try:
 			CryptSQL.__init__(self)
 
 			Cmd.__init__(self)
 			self.prompt = "pwman$ "
 
+			self.commitClearsUndo = commitClearsUndo
+			self.undo = UndoStack()
 			self.__openFile(filename, passphrase)
 		except (CSQLError), e:
 			raise PWMan.Error(str(e))
@@ -196,7 +248,11 @@ class PWMan(CryptSQL, Cmd):
 
 	def __err(self, source, message):
 		source = " " + source + ":" if source else ""
-		print "***%s %s\n" % (source, message)
+		raise self.CommandError("***%s %s" % (source, message))
+
+	def __info(self, source, message):
+		source = " " + source + ":" if source else ""
+		print "+++%s %s\n" % (source, message)
 
 	def precmd(self, line):
 		first = self.__getParam(line, 0, False, False)
@@ -205,10 +261,10 @@ class PWMan(CryptSQL, Cmd):
 		return line
 
 	def default(self, line):
-		self.__err(None, "Unknown command: %s" % line)
+		self.__err(None, "Unknown command: %s\nType 'help' for more help." % line)
 
 	def emptyline(self):
-		self.do_help([])
+		pass
 
 	def __complete_category_title(self, text, line, begidx, endidx):
 		# Generic [category] [title] completion
@@ -225,7 +281,7 @@ class PWMan(CryptSQL, Cmd):
 	cmdHelpMisc = (
 		("help", ("h",), "Show help about commands"),
 		("quit", ("q", "exit", "^D"), "Quit pwman"),
-		("cls", (), "Clear screen"),
+		("cls", (), "Clear screen and undo buffers"),
 	)
 
 	cmdHelpDatabase = (
@@ -241,6 +297,11 @@ class PWMan(CryptSQL, Cmd):
 		("edit_bulk", ("eb",), "Edit the 'bulk' field of an entry"),
 		("remove", ("rm", "del"), "Remove and existing entry"),
 		("dbdump", (), "Dump the database"),
+	)
+
+	cmdHelpHist = (
+		("undo", (), "Undo the last command"),
+		("redo", (), "Redo the last undone command"),
 	)
 
 	def do_help(self, params):
@@ -266,6 +327,8 @@ class PWMan(CryptSQL, Cmd):
 		printCmdHelp(self.cmdHelpDatabase)
 		print "\nEditing/listing commands:"
 		printCmdHelp(self.cmdHelpEdit)
+		print "\nHistory commands:"
+		printCmdHelp(self.cmdHelpHist)
 		print "\nType 'command?' or 'help command' for more help on a command."
 	do_h = do_help
 
@@ -282,18 +345,22 @@ class PWMan(CryptSQL, Cmd):
 	do_EOF = do_quit
 
 	def do_cls(self, params):
-		"""--- Clear console screen ---
+		"""--- Clear console screen and undo/redo buffer ---
 		Command: cls\n
-		Clear the console screen. Note that this does not clear
-		advanced buffers like the screen session buffer.\n
+		Clear the console screen and all undo/redo buffers.
+		Note that this does not clear a possibly existing
+		'screen' session buffer or other advanced console buffers.\n
 		Aliases: None"""
 		clearScreen()
+		self.undo.clear()
 
 	def do_commit(self, params):
 		"""--- Write changes to the database file ---\n
 		Command: commit\n
 		Aliases: c w"""
 		self.commit()
+		if self.commitClearsUndo:
+			self.undo.clear()
 	do_c = do_commit
 	do_w = do_commit
 
@@ -316,6 +383,7 @@ class PWMan(CryptSQL, Cmd):
 		if p != self.passphrase:
 			self.passphrase = p
 			self.setDirty()
+			self.undo.clear()
 
 	def do_list(self, params):
 		"""--- Print a listing ---
@@ -367,19 +435,40 @@ class PWMan(CryptSQL, Cmd):
 		if not category or not title:
 			self.__err("new", "Invalid parameters. "
 				"Need to supply category and title.")
-			return
 		entry = PWManEntry(category, title, user, pw, bulk)
 		try:
 			self.addEntry(entry)
 		except (self.Error), e:
 			self.__err("new", str(e))
-			return
+		self.undo.do("new %s" % params,
+			     "remove %s %s" % (escapeCmd(category), escapeCmd(title)))
 	do_n = do_new
 	do_add = do_new
 
 	complete_new = __complete_category_title
 	complete_n = complete_new
 	complete_add = complete_new
+
+	def __do_edit_generic(self, params, commandName,
+			      entry2data, data2entry):
+		category = self.__getParam(params, 0)
+		title = self.__getParam(params, 1)
+		if not category or not title:
+			self.__err(commandName, "Invalid parameters. "
+				"Need to supply category and title.")
+		oldEntry = self.getEntry(PWManEntry(category, title))
+		if not oldEntry:
+			self.__err(commandName, "Entry does not exist")
+		newData = self.__skipParams(params, 2).strip()
+		try:
+			self.editEntry(data2entry(category, title, newData))
+		except (self.Error), e:
+			self.__err(commandName, str(e))
+		self.undo.do("%s %s" % (commandName, params),
+			     "%s %s %s %s" %\
+			     (commandName, escapeCmd(oldEntry.category),
+			      escapeCmd(oldEntry.title),
+			      escapeCmd(entry2data(oldEntry))))
 
 	def do_edit_user(self, params):
 		"""--- Edit the 'user' field of an existing entry ---
@@ -389,19 +478,9 @@ class PWMan(CryptSQL, Cmd):
 		The NEWDATA must _not_ be escaped (however, category and
 		title must be escaped).\n
 		Aliases: eu"""
-		category = self.__getParam(params, 0)
-		title = self.__getParam(params, 1)
-		if not category or not title:
-			self.__err("edit_user", "Invalid parameters. "
-				"Need to supply category and title.")
-			return
-		newUser = self.__skipParams(params, 2).strip()
-		entry = PWManEntry(category, title, user=newUser)
-		try:
-			self.editEntry(entry)
-		except (self.Error), e:
-			self.__err("edit_user", str(e))
-			return
+		self.__do_edit_generic(params, "edit_user",
+			lambda entry: entry.user,
+			lambda cat, tit, data: PWManEntry(cat, tit, user=data))
 	do_eu = do_edit_user
 
 	def complete_edit_user(self, text, line, begidx, endidx):
@@ -429,19 +508,9 @@ class PWMan(CryptSQL, Cmd):
 		The NEWDATA must _not_ be escaped (however, category and
 		title must be escaped).\n
 		Aliases: ep"""
-		category = self.__getParam(params, 0)
-		title = self.__getParam(params, 1)
-		if not category or not title:
-			self.__err("edit_pw", "Invalid parameters. "
-				"Need to supply category and title.")
-			return
-		newPw = self.__skipParams(params, 2).strip()
-		entry = PWManEntry(category, title, pw=newPw)
-		try:
-			self.editEntry(entry)
-		except (self.Error), e:
-			self.__err("edit_pw", str(e))
-			return
+		self.__do_edit_generic(params, "edit_pw",
+			lambda entry: entry.pw,
+			lambda cat, tit, data: PWManEntry(cat, tit, pw=data))
 	do_ep = do_edit_pw
 
 	def complete_edit_pw(self, text, line, begidx, endidx):
@@ -463,25 +532,15 @@ class PWMan(CryptSQL, Cmd):
 
 	def do_edit_bulk(self, params):
 		"""--- Edit the 'bulk' field of an existing entry ---
-		Command: edit_com category title NEWDATA...\n
+		Command: edit_bulk category title NEWDATA...\n
 		Change the 'bulk' field of an existing database entry.
 		NEWDATA is the new data to write into the 'bulk' field.
 		The NEWDATA must _not_ be escaped (however, category and
 		title must be escaped).\n
-		Aliases: ec"""
-		category = self.__getParam(params, 0)
-		title = self.__getParam(params, 1)
-		if not category or not title:
-			self.__err("edit_com", "Invalid parameters. "
-				"Need to supply category and title.")
-			return
-		newBulk = self.__skipParams(params, 2).strip()
-		entry = PWManEntry(category, title, bulk=newBulk)
-		try:
-			self.editEntry(entry)
-		except (self.Error), e:
-			self.__err("edit_com", str(e))
-			return
+		Aliases: eb"""
+		self.__do_edit_generic(params, "edit_bulk",
+			lambda entry: entry.bulk,
+			lambda cat, tit, data: PWManEntry(cat, tit, bulk=data))
 	do_eb = do_edit_bulk
 
 	def complete_edit_bulk(self, text, line, begidx, endidx):
@@ -511,13 +570,18 @@ class PWMan(CryptSQL, Cmd):
 		if not category or not title:
 			self.__err("remove", "Invalid parameters. "
 				"Need to supply category and title.")
-			return
-		entry = PWManEntry(category, title)
+		oldEntry = self.getEntry(PWManEntry(category, title))
+		if not oldEntry:
+			self.__err("remove", "Entry does not exist")
 		try:
-			self.delEntry(entry)
+			self.delEntry(PWManEntry(category, title))
 		except (self.Error), e:
 			self.__err("remove", str(e))
-			return
+		self.undo.do("remove %s" % params,
+			     "new %s %s %s %s %s" %\
+			     (escapeCmd(oldEntry.category), escapeCmd(oldEntry.title),
+			      escapeCmd(oldEntry.user), escapeCmd(oldEntry.pw),
+			      escapeCmd(oldEntry.bulk)))
 	do_rm = do_remove
 	do_del = do_remove
 
@@ -542,7 +606,39 @@ class PWMan(CryptSQL, Cmd):
 			fd.flush()
 		except (IOError), e:
 			self.__err("dbdump", "Failed to write dump: %s" % e.strerror)
-			return
+
+	def do_undo(self, params):
+		"""--- Undo the last command ---
+		Command: undo\n
+		Rewinds the last command that changed the database.\n
+		Aliases: None"""
+		cmd = self.undo.undo()
+		if not cmd:
+			self.__err("undo", "There is no command to be undone.")
+		self.undo.freeze()
+		try:
+			self.onecmd(cmd.undoCommand)
+		finally:
+			self.undo.thaw()
+		self.__info("undo", cmd.doCommand + "\nsuccessfully undone with\n" +\
+			    cmd.undoCommand)
+
+	def do_redo(self, params):
+		"""--- Redo the last undone command ---
+		Command: redo\n
+		Redoes the last undone command.
+		Also see 'undo' help.\n
+		Aliases: None"""
+		cmd = self.undo.redo()
+		if not cmd:
+			self.__err("redo", "There is no undone command to be redone.")
+		self.undo.freeze()
+		try:
+			self.onecmd(cmd.doCommand)
+		finally:
+			self.undo.thaw()
+		self.__info("redo", cmd.undoCommand + "\nsuccessfully redone with\n" +\
+			    cmd.doCommand)
 
 	def __skipParams(self, line, count, lineIncludesCommand=False):
 		# Return a parameter string with the first 'count'
@@ -691,9 +787,9 @@ class PWMan(CryptSQL, Cmd):
 				break
 			except (self.Quit), e:
 				if self.__mayQuit():
-					clearScreen()
+					self.do_cls("")
 					break
-			except (EscapeError), e:
+			except (EscapeError, self.CommandError), e:
 				stdout(str(e) + "\n")
 			except (KeyboardInterrupt, EOFError), e:
 				stdout("\n")
