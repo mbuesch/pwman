24209991056ca8e564b1eb974e9bae019ba22f50
Michael Buesch <m@bues.ch>
2019-08-27 20:20:08 +0200

    Add windows support scripts
    Signed-off-by: Michael Buesch <m@bues.ch>

---
+++ pwman.cmd
@@ -0,0 +1,45 @@
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+
+set PYPROG=pwman
+
+
+set PATH=%PATH%;C:\WINDOWS;C:\WINDOWS\SYSTEM32
+for /D %%f in ( "C:\PYTHON*" ) do set PATH=!PATH!;%%f
+for /D %%f in ( "%USERPROFILE%\AppData\Local\Programs\Python\Python*" ) do set PATH=!PATH!;%%f;%%f\Scripts
+
+
+py -h >NUL 2>&1
+if %ERRORLEVEL% EQU 0 goto exec_py
+
+python3 -h >NUL 2>&1
+if %ERRORLEVEL% EQU 0 goto exec_python3
+
+python -h >NUL 2>&1
+if %ERRORLEVEL% EQU 0 goto exec_python
+
+echo Did not find Python 3.x in the PATH.
+echo Please make sure Python 3.x is installed correctly.
+pause
+goto end
+
+
+:exec_py
+@echo on
+py -3 -B %PYPROG% %1 %2 %3 %4 %5 %6 %7 %8 %9
+@goto end
+
+
+:exec_python3
+@echo on
+python3 -B %PYPROG% %1 %2 %3 %4 %5 %6 %7 %8 %9
+@goto end
+
+
+:exec_python
+@echo on
+python -B %PYPROG% %1 %2 %3 %4 %5 %6 %7 %8 %9
+@goto end
+
+
+:end
