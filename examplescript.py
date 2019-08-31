865a3d184eb8fe3bcd1c1ae78d27aeb24b51197b
Michael Buesch <m@bues.ch>
2019-08-31 18:37:38 +0200

    Add example script
    Signed-off-by: Michael Buesch <m@bues.ch>

---
+++ examplescript.py
@@ -0,0 +1,24 @@
+#
+# This is an example script that can be used as
+# pwman -p examplescript.py
+# pwman --call-pymod examplescript.py
+# See pwman --help for more information.
+#
+
+def run(db):
+	# Print all category names.
+	categories = db.getCategoryNames()
+	print("Categories:", categories)
+
+	# Print all titles in a category.
+	titles = db.getEntryTitles("testcat1")
+	print("Titles in testcat1:", titles)
+
+	# Move all titles from a category to another one and change the title.
+	for title in titles:
+		entry = db.getEntry("testcat1", title)
+		print("Moving entry:", entry)
+		db.moveEntry(entry, "othercat", "foobar_" + title)
+
+	# Permanently write the changes to the database file.
+	db.commit()
