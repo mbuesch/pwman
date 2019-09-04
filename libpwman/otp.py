a8f27ec76a32fc6a9e3b1e365b637bce28476e83
Michael Buesch <m@bues.ch>
2019-09-04 18:39:18 +0200

    otp: Remove dashes, underscores and spaces from hash name
    Signed-off-by: Michael Buesch <m@bues.ch>

--- libpwman/otp.py
+++ libpwman/otp.py
@@ -21,7 +21,12 @@ class OtpError(Exception):
 	pass
 
 def hotp(key, counter, nrDigits=6, hmacHash="SHA1"):
-	"""HOTP: An HMAC-Based One-Time Password Algorithm.
+	"""HOTP - An HMAC-Based One-Time Password Algorithm.
+	key: The HOTP key. Either raw bytes or a base32 encoded string.
+	counter: The HOTP counter integer.
+	nrDigits: The number of digits to return. Can be 1 to 8.
+	hmacHash: The name string of the hashing algorithm.
+	Returns the calculated HOTP token string.
 	"""
 	if isinstance(key, str):
 		try:
@@ -32,12 +37,17 @@ def hotp(key, counter, nrDigits=6, hmacHash="SHA1"):
 		raise OtpError("Invalid counter.")
 	if not (1 <= nrDigits <= 8):
 		raise OtpError("Invalid number of digits.")
+
 	try:
-		{
+		hmacHash = hmacHash.replace("-", "")
+		hmacHash = hmacHash.replace("_", "")
+		hmacHash = hmacHash.replace(" ", "")
+		hmacHash = hmacHash.upper().strip()
+		hmacHash = {
 			"SHA1"   : hashlib.sha1,
 			"SHA256" : hashlib.sha256,
 			"SHA512" : hashlib.sha512,
-		}[hmacHash.upper().strip()]
+		}[hmacHash]
 	except KeyError:
 		raise OtpError("Invalid HMAC hash type.")
 
@@ -60,7 +70,11 @@ def hotp(key, counter, nrDigits=6, hmacHash="SHA1"):
 	return fmt % otp
 
 def totp(key, nrDigits=6, hmacHash="SHA1", t=None):
-	"""TOTP: Time-Based One-Time Password Algorithm.
+	"""TOTP - Time-Based One-Time Password Algorithm.
+	nrDigits: The number of digits to return. Can be 1 to 8.
+	hmacHash: The name string of the hashing algorithm.
+	t: Optional; the time in seconds. Uses time.time(), if not given.
+	Returns the calculated TOTP token string.
 	"""
 	if t is None:
 		t = time.time()
