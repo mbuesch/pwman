824a2af02ce5b2aa0b92a1dbf0a88c8c5a489ef0
Michael Buesch <m@bues.ch>
2021-09-02 21:12:36 +0200

    Bump version

--- libpwman/version.py
+++ libpwman/version.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 # Simple password manager
-# Copyright (c) 2011-2019 Michael Buesch <m@bues.ch>
+# Copyright (c) 2011-2021 Michael Buesch <m@bues.ch>
 # Licensed under the GNU/GPL version 2 or later.
 """
 
@@ -13,7 +13,7 @@ __all__ = [
 ]
 
 VERSION_MAJOR = 2
-VERSION_MINOR = 4
+VERSION_MINOR = 5
 VERSION_EXTRA = ""
 
 VERSION_STRING = "%d.%d%s" % (VERSION_MAJOR, VERSION_MINOR, VERSION_EXTRA)
