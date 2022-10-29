5751c795c55f166a959f823ac271f2d1d668f63e
Michael Buesch <m@bues.ch>
2022-10-29 19:48:54 +0200

    release script: Small cleanups

--- maintenance/makerelease.sh
+++ maintenance/makerelease.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-srcdir="$(dirname "$0")"
-[ "$(echo "$srcdir" | cut -c1)" = '/' ] || srcdir="$PWD/$srcdir"
+srcdir="$(realpath -e "$0" | xargs dirname)"
 
 srcdir="$srcdir/.."
 
 die() { echo "$*"; exit 1; }
 
 # Import the makerelease.lib
-# http://bues.ch/gitweb?p=misc.git;a=blob_plain;f=makerelease.lib;hb=HEAD
+# https://bues.ch/cgit/misc.git/tree/makerelease.lib
 for path in $(echo "$PATH" | tr ':' ' '); do
 	[ -f "$MAKERELEASE_LIB" ] && break
 	MAKERELEASE_LIB="$path/makerelease.lib"
